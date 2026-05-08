bcac4f17d8be4f063d074d8f3944eae5ce6e8766
Léandre Simon <leandre.simon@gmail.com>
2026-05-08 17:09:59 +0200

    removed useless data from tuto

--- src/data/countries-10m.json.js
+++
@@ -1,9 +0,0 @@
-async function json(url) {
-    const response = await fetch(url);
-    if (!response.ok) throw new Error(`fetch failed: ${response.status}`);
-    return await response.json();
-}
-
-const topojson = await json("https://cdn.jsdelivr.net/npm/world-atlas@2/countries-10m.json");
-
-process.stdout.write(JSON.stringify(topojson));
